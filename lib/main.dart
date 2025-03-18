3c966a6c7b26d8f4196a4942626497d55600893a
KWAYU MMARI <84182991+kwayummari@users.noreply.github.com>
2025-03-18 16:06:36 +0300

    fixing main.dart

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:daladala_smart_app/providers/auth_provider.dart';
+import 'package:daladala_smart_app/providers/user_provider.dart';
+import 'package:daladala_smart_app/providers/trip_provider.dart';
+import 'package:daladala_smart_app/providers/booking_provider.dart';
+import 'package:daladala_smart_app/providers/payment_provider.dart';
+import 'package:daladala_smart_app/screens/auth/login_screen.dart';
+import 'package:daladala_smart_app/screens/home/home_screen.dart';
+import 'package:daladala_smart_app/config/app_config.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => UserProvider()),
+        ChangeNotifierProvider(create: (_) => TripProvider()),
+        ChangeNotifierProvider(create: (_) => BookingProvider()),
+        ChangeNotifierProvider(create: (_) => PaymentProvider()),
+      ],
+      child: Consumer<AuthProvider>(
+        builder: (context, authProvider, _) {
+          return MaterialApp(
+            title: 'Daladala Smart',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              primarySwatch: Colors.green,
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+              fontFamily: 'Poppins',
+              appBarTheme: const AppBarTheme(
+                color: AppColors.primary,
+                elevation: 0,
+              ),
+              elevatedButtonTheme: ElevatedButtonThemeData(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColors.primary,
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+              textButtonTheme: TextButtonThemeData(
+                style: TextButton.styleFrom(
+                  foregroundColor: AppColors.primary,
+                ),
+              ),
+            ),
+            home: authProvider.isAuthenticated
+                ? const HomeScreen()
+                : const LoginScreen(),
+            routes: {
+              '/login': (context) => const LoginScreen(),
+              '/home': (context) => const HomeScreen(),
+              // Add other routes as needed
+            },
+          );
+        },
+      ),
+    );
+  }
+}
