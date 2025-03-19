2f9943784212d474c522c360067065b38abd46ee
KWAYU MMARI <84182991+kwayummari@users.noreply.github.com>
2025-03-19 13:47:40 +0300

    fixing configurations files

--- lib/features/trips/data/models/trip_model.dart
+++ lib/features/trips/data/models/trip_model.dart
@@ -1,5 +1,5 @@
+import 'package:daladala_smart_app/features/trips/domains/entities/trip.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import '../../domain/entities/trip.dart';
 
 class TripModel extends Trip {
   const TripModel({
