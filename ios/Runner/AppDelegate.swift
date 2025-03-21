8291e18f4f4118a5944cec973b1119d6e08a8607
KWAYU MMARI <84182991+kwayummari@users.noreply.github.com>
2025-03-21 03:50:46 +0300

    fixing configurations files

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBH7beAoWGUJcMbG7j5e365zG6vzfQFyE8")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
